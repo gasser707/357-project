7f098df19a3b3fc03fa634ad648a60b4f5f85699
Khairo <khairo.khatib@gmail.com>
2022-04-20 23:05:29 -0400

    added 12th floor mapping (nodes and edges)

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import IndoorMap from "../components/IndoorMap";
 import fifthFloorPlan from "../data/floor_plans/fifthFloorPlan";
+import twelfthFloorPlan from "../data/floor_plans/twelfthFloorPlan";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
       <Head>
         <title>ConU Class Finder</title>
         <meta name="description" content="ConU Class Finder" />
@@ -14,18 +14,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-
-        <h1 className={styles.title}>
-          ConU Class Finder
-        </h1>
+        <h1 className={styles.title}>ConU Class Finder</h1>
 
         <IndoorMap
-          floorPlan={fifthFloorPlan} // currently hardcoded
-          startPoint="ES" // currently hardcoded
-          endPoint="_515" // currently hardcoded
+          floorPlan={twelfthFloorPlan} // currently hardcoded
+          startPoint="ESL" // currently hardcoded
+          endPoint="_1228" // currently hardcoded
         />
-
       </main>
     </div>
-  )
+  );
 }
